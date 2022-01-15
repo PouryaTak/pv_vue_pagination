390c4641430384305988c4235e320b9f71c4d265
pouriversal <pooriversal@gmail.com>
2022-01-15 20:56:08 +0330

    add page_input

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,5 +1,11 @@
 <template>
   <div v-if="paginationData.totalPages / paginationData.perPage > 1" class="pv_pagination">
+    <div class="pv_page_input" :class="pageInputToggler ? 'toggle_page_input':''">
+      <form @submit.prevent="jumptoPage">
+        <input ref="pv_input" type="number" v-model.number="page" />
+        <button type="submit" class="pv_btn">Go</button>
+      </form>
+    </div>
     <button class="pv_action_btn" :class="paginationData.currentPage == 1 ? 'disabled' : ''" @click="gotoPage(paginationData.currentPage - 1)">
       <svg class="chev_right" width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M1 1L5 5L9 1" stroke="#021C24" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
@@ -15,7 +21,7 @@
       <li v-for="i in 3" :key="i" class="pv_btn" :class="paginationData.currentPage == i ? 'active' : ''" @click="gotoPage(i)">
         {{ i }}
       </li>
-      <li class="pv_btn">...</li>
+      <li class="pv_btn" @click="pageInputToggle">...</li>
       <li v-for="i in threeLastPages" :key="i" class="pv_btn" :class="paginationData.currentPage == i ? 'active' : ''" @click="gotoPage(i)">
         {{ i }}
       </li>
@@ -26,7 +32,7 @@
         {{ i }}
       </li>
       <li class="pv_btn" @click="gotoPage(4)">4</li>
-      <li class="pv_btn">...</li>
+      <li class="pv_btn" @click="pageInputToggle">...</li>
       <li v-for="i in twoLastPages" :key="i" class="pv_btn" :class="paginationData.currentPage == i ? 'active' : ''" @click="gotoPage(i)">
         {{ i }}
       </li>
@@ -36,7 +42,7 @@
       <li v-for="i in 2" :key="i" class="pv_btn" :class="paginationData.currentPage == i ? 'active' : ''" @click="gotoPage(i)">
         {{ i }}
       </li>
-      <li class="pv_btn">...</li>
+      <li class="pv_btn" @click="pageInputToggle">...</li>
       <li class="pv_btn" @click="gotoPage(paginationData.totalPages - 3)">
         {{ paginationData.totalPages - 3 }}
       </li>
@@ -69,7 +75,7 @@
       <li class="pv_btn" @click="gotoPage(1)">
         {{ 1 }}
       </li>
-      <li class="pv_btn">...</li>
+      <li class="pv_btn" @click="pageInputToggle">...</li>
       <li class="pv_btn" @click="gotoPage(paginationData.currentPage - 1)">
         {{ paginationData.currentPage - 1 }}
       </li>
@@ -79,7 +85,7 @@
       <li class="pv_btn" @click="gotoPage(paginationData.currentPage + 1)">
         {{ paginationData.currentPage + 1 }}
       </li>
-      <li class="pv_btn">...</li>
+      <li class="pv_btn" @click="pageInputToggle">...</li>
       <li class="pv_btn" @click="gotoPage(paginationData.totalPages)">
         {{ paginationData.totalPages }}
       </li>
@@ -99,6 +105,12 @@ export default {
       type: Object,
       default: () => ({}),
     },
+  },
+  data() {
+    return {
+      page: "",
+      pageInputToggler: false,
+    };
   },
   computed: {
     threeLastPages() {
@@ -128,10 +140,38 @@ export default {
       }
       this.$emit("gotoPage", page);
     },
+    pageInputToggle() {
+      this.pageInputToggler = !this.pageInputToggler;
+      this.$refs.pv_input.focus();
+    },
+    jumptoPage() {
+      if (!this.page || this.page <= 0 || this.page > this.paginationData.totalPages) {
+        return;
+      }
+      this.gotoPage(this.page);
+      this.pageInputToggler = false;
+      this.page = "";
+    },
   },
 };
 </script>
 <style scoped>
+* {
+  box-sizing: border-box;
+    transition: all .3s ease;
+}
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+/* ------------------------------------ */
 .pv_pagination {
   /* css variables used here because of scoped style limitation */
   --text: #252525;
@@ -145,10 +185,8 @@ export default {
   align-items: center;
   width: max-content;
   margin: 0 auto;
-}
-
-.pv_pagination * {
   color: var(--text);
+  position: relative;
 }
 .pv_btn,
 .pv_action_btn {
@@ -163,6 +201,54 @@ export default {
   margin: 0 0.5rem;
   cursor: pointer;
   /* margin-left: 1.75rem; */
+}
+
+.pv_page_input {
+  width: max-content;
+  position: absolute;
+  border: var(--disable) 1px solid;
+  background: var(--white);
+  border-radius: 1rem;
+  padding: 0.5rem;
+  opacity: 0;
+  bottom: 0;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.07);
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: -1;
+}
+.pv_page_input form {
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+}
+
+.pv_page_input form button {
+  height: 2rem;
+  color: var(--white);
+  background: var(--text);
+}
+.pv_page_input form button:hover {
+  background: var(--active_color);
+}
+
+.pv_page_input form input {
+  width: 3rem;
+  height: 2rem;
+  font-size: 1rem;
+  text-align: center;
+  color: var(--text);
+  border: 1px solid var(--border);
+  border-radius: 0.5rem;
+}
+.pv_page_input form input:focus {
+  outline: none;
+  border: 1px solid var(--active_color);
+}
+
+.toggle_page_input {
+  bottom: -2.5rem;
+  opacity: 1;
 }
 
 .pv_btn {
@@ -209,5 +295,6 @@ export default {
 .pv_btn.active {
   background: var(--active_color);
   color: var(--white);
+  border: none;
 }
 </style>
